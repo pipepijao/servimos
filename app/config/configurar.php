ce9a91b0da35f1e7e67544f270ffb75fe1f30c4f
pipepijao <andres_140830@hotmail.com>
2021-06-29 00:34:45 -0500

    url

--- app/config/configurar.php
+++ app/config/configurar.php
@@ -1,4 +1,4 @@
 <?php
     define('RUTA_APP', dirname(dirname(__FILE__)));
-    define('RUTA_URL','http://localhost/servimos');
+    define('RUTA_URL','https://servimos.herokuapp.com/servimos');
     define('NOMBRESITIO','_NOMBRE_SITIO');
